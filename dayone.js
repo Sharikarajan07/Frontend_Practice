06e2225130aafcbcb731cca9b9975e63c2ff149a
Sharikarajan07 <shaarika.rajan@gmail.com>
2026-03-16 19:39:52 +0530

    Add initial JavaScript file with basic data types, functions, and structures

---
+++ dayone.js
@@ -0,0 +1,83 @@
+console.log(123);
+//datatype and variable
+console.log(typeof("sharika"));
+
+console.log(typeof(true));
+
+var a=23
+console.log(a);
+
+var b
+console.log(b);
+
+var c= null
+console.log(c);
+
+//operators
+
+console.log(2+2);
+console.log("sharika"+3);
+console.log("5"-2);
+console.log("6"*2);
+
+console.log(2>1);
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+//function
+function add(){
+    console.log(2+3);
+}
+add()
+
+//parameter and arguments
+function sharika(a,b){
+    console.log(a+b)
+}
+sharika(2,3)
+sharika(5,5)
+
+function welcome(name){
+    console.log(name, "welcome to our class");
+}
+welcome("sharika")
+welcome("aishu")
+welcome("kavi")
+
+//array
+var cars=["nexon","bmw","kia","duster"]
+console.log(cars);
+console.log(cars.length);
+console.log(cars[0]);
+
+//object
+var details = {
+    name:"sharika",
+    phone:584677889
+}
+console.log(details)
+console.log(details.name)
+
+// array and object
+
+var studentdetails= [
+    {
+        name:"sharika",
+        phone:4678755777
+    },
+    {
+        name:"kavi",
+        phone:4679976567
+    },
+    {
+        name:"aishu",
+        phone:679899997
+    }
+]
+console.log(studentdetails)
+console.log(studentdetails[1].name)
+
+
+
+
